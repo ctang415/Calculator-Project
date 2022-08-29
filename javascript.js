94ba5a3329832372fd761d6c1a60a0e251b57e72
ctang415 <carrietang117@gmail.com>
2022-08-28 23:40:29 -0400

    Created basic functions for calculator

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b){
+    return a * b;
+};
+
+const divide = function(a, b) {
+    if (a === 0 || b === 0) {
+        return 'ERROR';
+    }
+    else {
+    return a/b;
+    }
+};
+
+const clear = function() {
+    
+}
