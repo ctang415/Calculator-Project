ddd0685298e368b3597b9e6cda618a5cd416a479
ctang415 <carrietang117@gmail.com>
2022-09-01 13:02:37 -0400

    Created function for single operation calculation

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const displayContent = document.querySelector('.display');
-
+const displayContent = document.querySelector('.displayText');
+const displayOp = document.querySelector('.displayOp')
 
 let numberOne = '';
 let numberTwo = '';
@@ -9,10 +9,41 @@ let operator = '';
 const button = document.querySelectorAll('#button');
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', function() {
+        if (operator === '') {
         displayContent.textContent += this.textContent;
-        numberOne = displayContent.textContent;
+        numberOne += this.textContent;
+        console.log(numberOne)
+        }
+        else {
+            displayContent.textContent += this.textContent;
+            displayOp.textContent = '';
+            numberTwo = displayContent.textContent;
+            console.log(numberTwo)
+        }
+    });
+};
+
+
+const buttonOperate = document.querySelectorAll('#buttonOperate');
+    for (let i = 0; i < buttonOperate.length; i++) {
+        buttonOperate[i].addEventListener('click', function() {
+            if (numberOne === '') {
+                operator = false;
+            }
+            else {
+                displayContent.textContent = '';
+                displayOp.textContent = this.textContent;
+                operator = this.textContent;
+                console.log(operator)
+            }
         });
-    };
+};
+
+const buttonEqual = document.querySelector('#buttonEqual');
+buttonEqual.addEventListener('click', function() {
+    displayContent.textContent = operate(operator, parseInt(numberOne), parseInt(numberTwo));
+})
+
 
 function clearAll () {
     displayContent.textContent = '';
@@ -48,16 +79,13 @@ function divide(a, b) {
     }
 };
 
-function clear() {
-
-}
 
 function operate(operator, a, b) {
     switch(operator) {
         case "+":
             return add(a, b);
             break;
-        case  "-":
+        case "-":
             return subtract(a, b);
             break;
         case "*":
